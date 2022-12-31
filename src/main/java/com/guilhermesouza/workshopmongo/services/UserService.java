ccb11e31fe4af2ea2237c851428698a7184135b2
Guilherme Souza <guilherme.souza@datum.inf.br>
2022-12-31 10:15:13 -0300

    Conectando ao MongoDB com Repository e Service.

---
+++ src/main/java/com/guilhermesouza/workshopmongo/services/UserService.java
@@ -0,0 +1,18 @@
+package com.guilhermesouza.workshopmongo.services;
+
+import com.guilhermesouza.workshopmongo.domain.User;
+import com.guilhermesouza.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+}
