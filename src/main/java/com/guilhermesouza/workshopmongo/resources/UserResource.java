50eb31104ae00888ff148611c2e1d8bd44286bb8
Guilherme Souza <guilherme.souza@datum.inf.br>
2023-01-01 05:27:01 -0300

    Deleção de usuáro com delete

--- src/main/java/com/guilhermesouza/workshopmongo/resources/UserResource.java
+++ src/main/java/com/guilhermesouza/workshopmongo/resources/UserResource.java
@@ -5,11 +5,10 @@ import com.guilhermesouza.workshopmongo.dto.UserDTO;
 import com.guilhermesouza.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,4 +30,19 @@ public class UserResource {
         User user = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
     }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> inser(@RequestBody UserDTO objDto) {
+        User obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
+
--- src/main/java/com/guilhermesouza/workshopmongo/services/UserService.java
+++ src/main/java/com/guilhermesouza/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.guilhermesouza.workshopmongo.services;
 
 import com.guilhermesouza.workshopmongo.domain.User;
+import com.guilhermesouza.workshopmongo.dto.UserDTO;
 import com.guilhermesouza.workshopmongo.repository.UserRepository;
 import com.guilhermesouza.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,5 +25,18 @@ public class UserService {
     public User findById(String id) {
         Optional<User> OptionalUser = userRepository.findById(id);
         return OptionalUser.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return userRepository.insert(obj);
+    }
+
+    public void delete(String id) {
+        findById(id);
+        userRepository.deleteById(id);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
