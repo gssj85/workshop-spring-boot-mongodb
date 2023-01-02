1610c7477604f2e7436ee07b9a588e9e6256e5e2
Guilherme Souza <guilherme.souza@datum.inf.br>
2023-01-02 07:12:59 -0300

    Consulta simpels com query method.

--- src/main/java/com/guilhermesouza/workshopmongo/resources/PostResource.java
+++ src/main/java/com/guilhermesouza/workshopmongo/resources/PostResource.java
@@ -1,12 +1,12 @@
 package com.guilhermesouza.workshopmongo.resources;
 
 import com.guilhermesouza.workshopmongo.domain.Post;
+import com.guilhermesouza.workshopmongo.resources.util.URL;
 import com.guilhermesouza.workshopmongo.services.PostService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -21,6 +21,15 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = postService.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(
+            @RequestParam(value = "search", defaultValue = "") String text
+    ) {
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
 
--- src/main/java/com/guilhermesouza/workshopmongo/services/PostService.java
+++ src/main/java/com/guilhermesouza/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.guilhermesouza.workshopmongo.repository.PostRepository;
 import com.guilhermesouza.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +20,9 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> OptionalPost = postRepository.findById(id);
         return OptionalPost.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return postRepository.findByTitleContainingIgnoreCase(text);
     }
 }
